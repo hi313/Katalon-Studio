8cb23cf85eb9f68c833d848edcbb17bb812091c5
HiralHarnal <HiralHarnal@DE-GN-30721.mshome.net>
2020-06-26 22:05:21 +0200

    Rest API

---
+++ Test Suites/Rest API Test Suite/User Validation Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User Validation Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a6f2b4d6-5691-4e88-bc49-c94c5b8a7f42</testSuiteGuid>
+   <testCaseLink>
+      <guid>90830d67-dd42-413b-a942-cb4e6a9b7262</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rest API Test Case/Validate User Details</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
